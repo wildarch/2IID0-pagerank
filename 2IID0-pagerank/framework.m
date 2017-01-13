8d9cf49de50738485db652aa6be86a58cdb5eeda
wildarch <daandegraaf9@gmail.com>
2017-01-13 12:09:28 +0100

    Use A as baseline in value based error

--- 2IID0-pagerank/framework.m
+++ 2IID0-pagerank/framework.m
@@ -1,4 +1,5 @@
 A = load('transition.txt', '-ascii');
+A_pagerank = power_without_teleport(A);
 a = 1;
 B = []; %an empty matrix to grow
 X = []; %Will contain the pagerank for each iteration
@@ -7,8 +8,6 @@ random_edge_indices = randperm(length(A));
 while length(B) ~= length(A)
     B(a,:) = A(random_edge_indices(a),:);
     X(:,a) = power_without_teleport(B); %after every node added, calculate the pageRank and store it in a different row in X
+    V(a) = value_based_error(A_pagerank,X(:,a));
     a = a+1;
-    if a > 2
-        V(a) = value_based_error(B(a-1,:),B(a-2,:));
-    end
 end
