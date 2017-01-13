3d94c0c8ff56e1a91faa17fea7ae80a21288db5c
wildarch <daandegraaf9@gmail.com>
2017-01-13 11:07:39 +0100

    Fix the framework

--- 2IID0-pagerank/framework.m
+++ 2IID0-pagerank/framework.m
@@ -1,11 +1,14 @@
 A = load('transition.txt', '-ascii');
-% a = 1;
-% B = A * 0; %an empty matrix to grow
-% X = [];
-% while B ~= A
-%     randomEdgeNumber = randi(length(A));
-%     randomEdge = A(randomEdgeNumber,:);
-%     B(randomEdgeNumber,:) = randomEdge;
-%     X(b,:) = power_without_teleport(B); %after every node added, calculate the pageRank and store it in a different row in X
-% end
+a = 1;
+b = 1;
+B = []; %an empty matrix to grow
+X = [];
+while length(B) ~= length(A)
+    randomEdgeNumber = randi(length(A));
+    randomEdge = A(randomEdgeNumber,:);
+    B(a,:) = randomEdge;
+    a = a+1;
+    X(b,:) = power_without_teleport(B); %after every node added, calculate the pageRank and store it in a different row in X
+    b = b + 1;
+end
 value_based_error(A)
